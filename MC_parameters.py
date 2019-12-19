f4bd33f42149a8c16a77b519b5820d1af5e280e1
Peter Young <peter.young@york.ac.uk>
2019-12-19 21:23:53 +0000

    Set up to use test data

--- MC_parameters.py
+++ MC_parameters.py
@@ -8,14 +8,18 @@ be edited appropriately for your project.
  
 #Parameters that control the stringency of the analysis
 #These values will replace the defaults - comment them out to use the defaults
+
 read_diff = 2 #default = 2
 #Count seqid only if most abundant sequence has at least read_diff more reads than the next
+
 reject_threshold = 0.7 #default = 0.7
 #Reject sequences that occur as second sequences with seqids at least reject_threshold
 #times as often as they occur as the primary sequence
+
 add_limit = 0.001 #default = 0.001
 #sequences are included in rank order until the next would add a fraction less than add_limit
 #i.e. this discards sequences with an overall relative abundance less than add_limit
+
 output_types = 3 #default = 3
 #add output filtered on a per-sample basis if set to 4
 
@@ -28,7 +32,8 @@ seqid_len = 13
 f_primer_len = 26
 r_primer_len = 23
 total_len = 313
-working_folder = "/Users/peter/Documents/Sequence_data/MiSeq_2018-05-31_NCHAIN/assembled/recA/"
+working_folder = "test/" #Use the example data provided in the 'test' folder
+#working_folder = "/Users/peter/Documents/Sequence_data/MiSeq_2018-05-31_NCHAIN/assembled/recA/"
 #working_folder = "/Users/peter/Documents/Sequence_data/MiSeq_2019-01_NCHAIN/PEAR_assembled_synthseq_mix/assembled/recA/"
 #working_folder = "/Users/peter/Documents/Sequence_data/MiSeq_2019-01_NCHAIN/clovergeno_andphusion_samples/synth_seq_mix/recA/"
 #working_folder = "/Users/peter/Documents/Sequence_data/MiSeq_2019-01_NCHAIN/clovergeno_andphusion_samples/clover_geno/recA/"
